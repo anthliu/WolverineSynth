52763d267795624cae7732de6b1668d0c2f7662f
Anthony Liu <liu916@gmail.com>
2016-04-06 21:30:42 -0400

    fix all_track bug

--- base/all_track_to_sig.m
+++ base/all_track_to_sig.m
@@ -1,11 +1,10 @@
 function sig = all_track_to_sig(cumul, tm)
-{
     sig = 0;
     for track = 1:length(cumul)
         cur_sig = [];
         for note = cumul{track}
-            cur_sig = [sig write_note(note, tm)];
+            cur_sig = [cur_sig write_note(note, tm)];
         end
         sig = sig + cur_sig;
     end
-}
+end
