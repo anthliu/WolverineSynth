77c9c6df2e13189e2738b322c66e77cb4c928b03
Anthony Liu <liu916@gmail.com>
2016-04-07 01:29:13 -0400

    improve plot

--- base/update_plot.m
+++ base/update_plot.m
@@ -1,9 +1,15 @@
 function [] = update_plot(note_list)
+    max_notes = 20;
     freqs = [];
     for nts = note_list
         freqs = [freqs nts.freq];
     end
     notes = 4 + 12 * log2(freqs / 440);
-    stem(notes, 'filled');
-    axis([0 length(freqs) + 1 0 12]);
+    if length(notes) > 20
+        p_notes = notes(end - 19: end);
+    else
+        p_notes = notes;
+    end
+    stem(p_notes, 'filled');
+    axis([0 21 0 12]);
 end
