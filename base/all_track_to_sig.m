ec9b25f3ba7e99bf3eec1b04ac108d98df9d5ace
Anthony Liu <liu916@gmail.com>
2016-04-09 17:10:37 -0400

    fix play all tracks

--- base/all_track_to_sig.m
+++ base/all_track_to_sig.m
@@ -5,6 +5,12 @@ function sig = all_track_to_sig(cumul, tm)
         for note = cumul{track}
             cur_sig = [cur_sig write_note(note, tm)];
         end
+        if length(cur_sig) < length(sig)
+            cur_sig(length(sig)) = 0;
+        elseif length(cur_sig) > length(sig)
+            sig(length(cur_sig)) = 0;
+        end
+        whos
         sig = sig + cur_sig;
     end
 end
