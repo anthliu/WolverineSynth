7efeb1bdf0812069c949510a661017bb6dadc70e
Anthony Liu <liu916@gmail.com>
2016-04-09 23:28:41 -0400

    plot with time

--- base/update_plot.m
+++ base/update_plot.m
@@ -1,15 +1,20 @@
 function [] = update_plot(note_list)
-    max_notes = 20;
+    max_beats = 20;
     freqs = [];
+    times = 0;
     for nts = note_list
         freqs = [freqs nts.freq];
+        times = [times times(end)+nts.beats];
     end
+
+    times(end) = [];
+
     notes = 4 + 12 * log2(freqs / 440);
-    if length(notes) > 20
-        p_notes = notes(end - 19: end);
+
+    stem(times, notes, 'filled');
+    if (times(end) < 20)
+        axis([-1 21 0 12]);
     else
-        p_notes = notes;
+        axis([times(end) - 21 times(end)+1 0 12]);
     end
-    stem(p_notes, 'filled');
-    axis([0 21 0 12]);
 end
