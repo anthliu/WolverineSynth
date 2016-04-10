13434ac5a5ff9263d304c40dcab4e99f6cba6c89
Anthony Liu <liu916@gmail.com>
2016-04-10 14:13:52 -0400

    add variable length

--- base/classify.m
+++ base/classify.m
@@ -1,5 +1,5 @@
 function freqs = classify(signal, Fs, thresh)
-    candidates = 440 * 2.^[-3:1/12:4];
+    candidates = 440 * 2.^[-2:1/12:4];
 
     N = length(signal);
 
@@ -7,14 +7,17 @@ function freqs = classify(signal, Fs, thresh)
     spectro = spect(1:end / 2);
 
     scores = [];
+    %nm = 0;
     for cand = candidates
         c_mean = cand * N / Fs + 1;
         %c_dev = 0.008 * cand * N / Fs + 1;
-        c_dev = N / Fs;
+        c_dev = 2 * N / Fs;
         score = c_dev * normpdf([1:N / 2], c_mean, c_dev) * spectro';
         scores = [scores score];
+        %nm = nm + c_dev * normpdf([1:N / 2], c_mean, c_dev);
     end
 
     freqs = candidates(scores > thresh);
     %plot([0:N/2 - 1] * Fs / N, spectro)
+    %plot([0:N/2 - 1] * Fs / N, nm);
 end
